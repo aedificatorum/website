e153550d8fa956a7d1a61ed7cae3d5d5aaa1887e
celinechole <github@celinechole.com>
2019-05-13 16:54:47 -0400

    update about

--- website/src/About.js
+++ website/src/About.js
@@ -3,7 +3,9 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      Hello from About
+      <p>Aedificatorum</p>
+      <p>Genitive masculine plural of aedificator</p>
+      <p>Translation: builder, architect, maker</p>
     </div>
   )
 }
