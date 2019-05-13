5b1829121cc6a22e92316a94f809b1f85c7c5247
celinechole <github@celinechole.com>
2019-05-13 16:42:28 -0400

    style home logo

--- website/src/App.js
+++ website/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { NavLink, Route, Switch } from "react-router-dom";
+import "./App.css";
+import About from "./About";
 
 function App() {
   return (
     <div>
-      Hello
+      <img src="aedificatorum-logo.png" alt="aedificatorum-logo" className="logo-home"/>
+      <h1>We are Aedificatorum</h1>
+      <h2>/ɑədɪfɪkætɒrʊəm/</h2>
+      {/* <NavLink to="/about">
+        <h3>About</h3>
+      </NavLink> */}
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route exact path="/" />
+      </Switch>
     </div>
   );
 }
