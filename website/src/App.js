74b86cc7db3164ef5d898fb1db6fa0cbf01e8ba7
Tim Addison <git@tjaddison.com>
2019-05-13 20:32:05 -0400

    More ES6

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { NavLink, Route, Switch } from "react-router-dom";
 import "./App.css";
 import About from "./About";
 import {
@@ -12,119 +11,79 @@ import {
   scroller
 } from "react-scroll";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.scrollToTop = this.scrollToTop.bind(this);
-  }
-  componentDidMount() {
-    Events.scrollEvent.register("begin", function() {
-      console.log("begin", arguments);
-    });
+const App = () => {
 
-    Events.scrollEvent.register("end", function() {
-      console.log("end", arguments);
-    });
-  }
-  scrollToTop() {
+  const scrollToTop = () => {
     scroll.scrollToTop();
   }
-  scrollTo() {
+
+  const scrollTo = () => {
     scroller.scrollTo("scroll-to-element", {
       duration: 800,
       delay: 0,
       smooth: "easeInOutQuart"
     });
   }
-  scrollToWithContainer() {
-    let goToContainer = new Promise((resolve, reject) => {
-      Events.scrollEvent.register("end", () => {
-        resolve();
-        Events.scrollEvent.remove("end");
-      });
 
-      scroller.scrollTo("scroll-container", {
-        duration: 800,
-        delay: 0,
-        smooth: "easeInOutQuart"
-      });
-    });
+  return (
+    <div>
+      <img
+        src="aedificatorum-logo.png"
+        alt="aedificatorum-logo"
+        className="logo-home"
+      />
+      <p>lol the other p?</p>
+      <h1>We are Aedificatorum</h1>
+      <h2>/ɑədɪfɪkætɒrʊəm/</h2>
+      <a onClick={() => scrollTo()}>
+        <h3>About</h3>
+      </a>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
+      <p>spacer at the top</p>
 
-    goToContainer.then(() =>
-      scroller.scrollTo("scroll-container-second-element", {
-        duration: 800,
-        delay: 0,
-        smooth: "easeInOutQuart",
-        containerId: "scroll-container"
-      })
-    );
-  }
-  componentWillUnmount() {
-    Events.scrollEvent.remove("begin");
-    Events.scrollEvent.remove("end");
-  }
-
-  render() {
-    return (
-      <div>
-        <img
-          src="aedificatorum-logo.png"
-          alt="aedificatorum-logo"
-          className="logo-home"
-        />
-        <p>lol the other p?</p>
-        <h1>We are Aedificatorum</h1>
-        <h2>/ɑədɪfɪkætɒrʊəm/</h2>
-        <a onClick={() => this.scrollTo()}>
-          <h3>About</h3>
-        </a>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        <p>spacer at the top</p>
-        
-        <Element name="scroll-to-element" className="element">
-          <h1>About</h1>
-          <About />
-        </Element>
-        <p>spacer at the bottom</p>
-        <p>spacer at the bottom</p>
-        <p>spacer at the bottom</p>
-        <p>spacer at the bottom</p>
-        <p>spacer at the bottom</p>
-        <p>spacer at the bottom</p>
-        <p>spacer at the bottom</p>
-        <p>spacer at the bottom</p>
-        <p>spacer at the bottom</p>
-        <p>spacer at the bottom</p>
-        <p>spacer at the bottom</p>
-        <p>spacer at the bottom</p>
-        <p>spacer at the bottom</p>
-        <a onClick={this.scrollToTop}>To the top</a>
-      </div>
-    );
-  }
+      <Element name="scroll-to-element" className="element">
+        <h1>About</h1>
+        <About />
+      </Element>
+      <p>spacer at the bottom</p>
+      <p>spacer at the bottom</p>
+      <p>spacer at the bottom</p>
+      <p>spacer at the bottom</p>
+      <p>spacer at the bottom</p>
+      <p>spacer at the bottom</p>
+      <p>spacer at the bottom</p>
+      <p>spacer at the bottom</p>
+      <p>spacer at the bottom</p>
+      <p>spacer at the bottom</p>
+      <p>spacer at the bottom</p>
+      <p>spacer at the bottom</p>
+      <p>spacer at the bottom</p>
+      <a onClick={scrollToTop}>To the top</a>
+    </div>
+  );
 }
 
 export default App;
