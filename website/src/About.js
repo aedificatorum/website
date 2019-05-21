ee42f815c5054762a4c2b6e3b0c6a21197f697ac
celinechole <github@celinechole.com>
2019-05-21 15:07:54 -0600

    remove text, add bottom padding

--- website/src/About.js
+++ website/src/About.js
@@ -3,11 +3,20 @@ import React from "react";
 const About = () => {
   return (
     <div className="about">
-      <p>Aedificatorum</p>
-      <p>Genitive masculine plural of aedificator</p>
-      <p>Translation: builder, architect, maker</p>
+      <div className="row">
+        <div className="col m6 s12">
+          <p>Aedificatorum</p>
+          <p>
+            In Latin aedificatorum is the genitive masculine plural of
+            aedificator.
+          </p>
+          <p>Genetive is the case that expresses possesion.</p>
+          <p>Translation: builder, architect, maker</p>
+        </div>
+        <div className="col m6 s12">Aedificatorum is Tim and Céline ...</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default About;
