6f2bad7fb9a94a2ac4d9bae48b176a34aaf8ad09
celinechole <github@celinechole.com>
2019-05-21 16:30:16 -0600

    format document

--- website/src/Footer.js
+++ website/src/Footer.js
@@ -1,24 +1,22 @@
 import React from "react";
 
-const Footer = (props) => {
+const Footer = props => {
   const { scrollToTop } = props;
 
   return (
     <footer className="footer mt-auto">
-    <div className="py-3">
+      <div className="py-3">
         <span className="text-muted">
-        <a href="/" onClick={scrollToTop}><i class="fas fa-chevron-up"></i></a>
-       
-        <div>LINKS</div>
+          <a href="/" onClick={scrollToTop}>
+            <i class="fas fa-chevron-up" />
+          </a>
+          <div>LINKS</div>
         </span>
-
       </div>
-        
       <div className="footer-bottom py-3">
         <span className="text-light">
-        <div>Built & Designed by Aedificatorum.</div>
+          <div>Built & Designed by Aedificatorum.</div>
         </span>
-
       </div>
     </footer>
   );
