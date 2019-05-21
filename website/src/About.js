4b287f33ab26ced294552429360e74e56effe83f
celinechole <github@celinechole.com>
2019-05-21 15:23:51 -0600

    more styling

--- website/src/About.js
+++ website/src/About.js
@@ -2,18 +2,22 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="about">
+    <div className="about-page">
       <div className="row">
         <div className="col-4 offset-md-1">
-          <h4 className="about-translation">Builder - Architect Maker</h4>
-          <p>
+          <h4 className="about-content">Builder - Architect Maker</h4>
+          <h5>
             In Latin <i>aedificatorum</i> is the genitive masculine plural of
             aedificator.
-          </p>
-          <p>Genetive is the case that expresses possesion.</p>
+          </h5>
+          <h5>Genetive is the case that expresses possesion.</h5>
         </div>
         <div className="col-6 offset-md-1">
-          Aedificatorum is Tim and Céline ...
+          <h4 className="about-content">Who are we?</h4>
+          <h5><b>Aedificatorum</b> is a shared project created by Céline & Tim.</h5>
+
+          <h4>Tim</h4>
+          <h4>Céline</h4>
         </div>
       </div>
     </div>
