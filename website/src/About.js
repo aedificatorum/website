9e5b6f27294e7a1d4007ff7ed32fb306c6fc9999
celinechole <github@celinechole.com>
2019-05-21 15:18:37 -0600

    update layout

--- website/src/About.js
+++ website/src/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className="about">
       <div className="row">
-        <div className="col-4">
+        <div className="col-4 offset-md-1">
           <h4 className="about-translation">Builder - Architect Maker</h4>
           <p>
             In Latin <i>aedificatorum</i> is the genitive masculine plural of
@@ -12,7 +12,7 @@ const About = () => {
           </p>
           <p>Genetive is the case that expresses possesion.</p>
         </div>
-        <div className="col-6 offset-md-2">
+        <div className="col-6 offset-md-1">
           Aedificatorum is Tim and Céline ...
         </div>
       </div>
