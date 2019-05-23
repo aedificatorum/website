10ccf1bf5e256fc70427460c49ab8e8c3d01bacf
celinechole <github@celinechole.com>
2019-05-23 11:21:58 -0600

    chevron in the middle

--- website/src/Footer.js
+++ website/src/Footer.js
@@ -5,20 +5,23 @@ const Footer = props => {
 
   return (
     <footer className="footer mt-auto">
-      <div className="py-3">
-        <span className="text-muted">
+        <div className="chevron mr-3">
           <a href="/" onClick={scrollToTop}>
             <i className="fas fa-chevron-up" />
           </a>
-          <div>
-            <a href="https://github.com/aedificatorum" target="blank">
-              <i className="fab fa-github-square fa-3x" />
-            </a>
-            <a href="https://twitter.com/aedificatorum" target="blank">
-              <i class="fab fa-twitter-square fa-3x" />
-            </a>
-          </div>
-        </span>
+        </div>
+      <div className="py-3">
+        <div className="text-muted">
+          <a href="https://github.com/aedificatorum" target="blank">
+            <i className="fab fa-github-square fa-3x px-2" />
+          </a>
+          <a href="https://twitter.com/aedificatorum" target="blank">
+            <i className="fab fa-twitter-square fa-3x px-2" />
+          </a>
+          <a href="https://twitter.com/aedificatorum" target="blank">
+            <i className="fas fa-square fa-3x px-2" />
+          </a>
+        </div>
       </div>
       <div className="footer-bottom py-3">
         <span className="text-light">
