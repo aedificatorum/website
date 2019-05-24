28cec57be4120682fd05fc2a28b74f82a0ed6da2
celinechole <github@celinechole.com>
2019-05-24 17:50:54 -0600

    fix target in footer

--- website/src/Footer.js
+++ website/src/Footer.js
@@ -6,13 +6,13 @@ const Footer = () => {
     <footer className="footer mt-auto">
       <div className="py-5">
         <div className="text-center">
-          <a href="https://github.com/aedificatorum" target="blank">
+          <a href="https://github.com/aedificatorum" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-github fa-2x px-3" />
           </a>
-          <a href="https://twitter.com/aedificatorum" target="blank">
+          <a href="https://twitter.com/aedificatorum" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-twitter fa-2x px-3" />
           </a>
-          <a href="https://twitter.com/aedificatorum" target="blank">
+          <a href="https://twitter.com/aedificatorum" target="_blank" rel="noopener noreferrer">
             <i className="fas fa-square fa-2x px-3" />
           </a>
         </div>
